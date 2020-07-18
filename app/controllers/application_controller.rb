38d68daa8b10d85e6684df4f94f245b3ae1c74fc
OrkunSA <c.orkun.s@gmail.com>
2020-07-18 12:56:08 -0500

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect to '/students/index'
+    else
+    erb :index
+    end
+  end
+
+  helpers do 
+
+    def logged_in? 
+      !!session[:user_id]
+    end
+
+    def current_user
+      Student.find_by_id(session[:user_id])
+    end
   end
 
 end
