7965bf4880cc6c01ede03e43052fe97b98cf66cb
OrkunSA <c.orkun.s@gmail.com>
2020-07-18 12:17:43 -0500

    added new controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    register Sinatra::Flash
+    enable :sessions
+    set :session_secret, "Yabba Dabba Doo"
   end
 
   get "/" do
