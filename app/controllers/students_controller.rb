f8f8eb246ddaac1bf642dce952586850db7d8e4a
OrkunSA <c.orkun.s@gmail.com>
2020-07-20 16:54:39 -0500

    Students controller in progress

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,9 @@
 class StudentsController < ApplicationController
+    get '/signup' do 
+        if  logged_in?
+            redirect to '/projects'
+        else
+            erb :'students/signup'
+        end
+    end
 end
